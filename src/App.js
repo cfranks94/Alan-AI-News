6bd4374ec5143e3eef48d8d141d0ccf6d37e58fe
Cord Franks <crfranks94@gmail.com>
2020-10-15 23:54:52 -0500

    complete instructional homepage components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles';
 
 const alanKey =
   "55160c436dae2d2f421b688efc83d4192e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
     const [newsArticles, setNewsArticles] = useState([]);
+    const classes = useStyles();
 
     useEffect(() => {
         alanBtn({
@@ -22,7 +24,9 @@ const App = () => {
 
     return (
         <div>
-            <h1>Alan AI News Application</h1>
+            <div className={classes.logoContainer}>
+                <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="alan logo" />
+            </div>
             <NewsCards articles={newsArticles} />
         </div>
     )
